ec1531321c70058cecda0f1313b8edd10c5fb134
vtrifonov548 <vtrifonov548@gmail.com>
2020-04-07 03:33:18 +0300

    Add autofac DI

---
+++ EducationalHelp.Domain/ServicesAutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace EducationalHelp.Services
+{
+    public class ServicesAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).Where(t => t.Name.EndsWith("Service")).AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
