77d3f2f2e46386496f627eed7c880e24cd0df546
vtrifonov548 <vtrifonov548@gmail.com>
2020-04-07 03:31:48 +0300

    Some fixes and improvements

---
+++ EducationalHelp.Data/Mapping/IDataMapper.cs
@@ -0,0 +1,14 @@
+﻿using EducationalHelp.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalHelp.Data.Mapping
+{
+    public interface IDataMapper<Entity> where Entity : BaseEntity
+    {
+        void Map(EntityTypeBuilder<Entity> builder);
+    }
+}
---
+++ EducationalHelp.Data/Mapping/Subjects/SubjectsMapping.cs
@@ -0,0 +1,25 @@
+﻿using EducationalHelp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalHelp.Data.Mapping.Subjects
+{
+    internal class SubjectsMapping : IDataMapper<Subject>
+    {
+        public void Map(EntityTypeBuilder<Subject> builder)
+        {
+            builder.ToTable("Subjects");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(50).IsRequired();
+            builder.Property(t => t.Description);
+            builder.Property(t => t.Teacher).HasMaxLength(50);
+            builder.HasOne(t => t.Annotation).WithOne().HasForeignKey<Subject>(t => t.AnnotationId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(t => t.Program).WithOne().HasForeignKey<Subject>(t => t.ProgramId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(t => t.ValuationTools).WithOne().HasForeignKey<Subject>(t => t.ValuationToolsId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasMany(t => t.Books).WithOne(t => t.Subject).HasForeignKey(t => t.SubjectId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
