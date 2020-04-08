6ef862b0a5582872b1fb91c77caf9cde873822cd
vtrifonov548 <vtrifonov548@gmail.com>
2020-04-08 05:31:19 +0300

    Update mapping in ef core

--- EducationalHelp.Core/Entities/Subject.cs
+++ EducationalHelp.Core/Entities/Subject.cs
@@ -10,15 +10,15 @@ namespace EducationalHelp.Core.Entities
         public string Description { get; set; }
         public string Teacher { get; set; }
 
-        public File Annotation { get; set; }
-        public Guid AnnotationId { get; set; }
+        public virtual File Annotation { get; set; }
+        public Guid? AnnotationId { get; set; }
 
-        public File Program { get; set; }
-        public Guid ProgramId { get; set; }
+        public virtual File Program { get; set; }
+        public Guid? ProgramId { get; set; }
 
-        public File ValuationTools { get; set; }
-        public Guid ValuationToolsId { get; set; }
+        public virtual File ValuationTools { get; set; }
+        public Guid? ValuationToolsId { get; set; }
 
-        public List<Book> Books { get; set; }
+        public virtual List<Book> Books { get; set; }
     }
 }
--- EducationalHelp.Data/Mapping/Subjects/SubjectsMapping.cs
+++ EducationalHelp.Data/Mapping/Subjects/SubjectsMapping.cs
@@ -17,7 +17,7 @@ namespace EducationalHelp.Data.Mapping.Subjects
             builder.Property(t => t.Description);
             builder.Property(t => t.Teacher).HasMaxLength(50);
             builder.HasOne(t => t.Annotation).WithOne().HasForeignKey<Subject>(t => t.AnnotationId).OnDelete(DeleteBehavior.NoAction);
-            builder.HasOne(t => t.Program).WithOne().HasForeignKey<Subject>(t => t.ProgramId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(t => t.Program).WithOne().HasForeignKey<Subject>(t => t.ProgramId).IsRequired(false);
             builder.HasOne(t => t.ValuationTools).WithOne().HasForeignKey<Subject>(t => t.ValuationToolsId).OnDelete(DeleteBehavior.NoAction);
             builder.HasMany(t => t.Books).WithOne(t => t.Subject).HasForeignKey(t => t.SubjectId).OnDelete(DeleteBehavior.Cascade);
         }
