3be32dc4409a7d2b663fe6d70ba53fd466ed4989
vtrifonov548 <vtrifonov548@gmail.com>
2020-10-17 00:10:56 +0300

    Обновление конфигурации маршрутов

--- EducationalHelp.Client/src/router.ts
+++ EducationalHelp.Client/src/router.ts
@@ -4,6 +4,7 @@ import MainPageComponent from "@/pages/MainPage/index.vue";
 import Error404PageComponent from "@/pages/Error404Page/index.vue";
 import SubjectsPageComponent from "@/pages/SubjectsPage/index.vue";
 import SubjectViewPageComponent from "@/pages/SubjectsViewPage/index.vue";
+import LessonSinglePage from "@/pages/LessonSinglePage/index.vue";
 
 Vue.use(VueRouter)
 
@@ -11,7 +12,9 @@ const routes = [
     { name: 'homePage', path: '/', component: MainPageComponent },
     { name: 'error404', path: '*', component: Error404PageComponent },
     { name: 'subjectsList', path: '/subjects', component: SubjectsPageComponent },
-    { name: 'subjectView', path: '/subjects/view.:id', component: SubjectViewPageComponent }
+    { name: 'subjectView', path: '/subjects/v-:id', component: SubjectViewPageComponent },
+    { name: 'lessonView', path: '/subjects/:subjectId-l-:lessonId', component: LessonSinglePage }
+
 ];
 
 export default new VueRouter((
