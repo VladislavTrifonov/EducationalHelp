382367a6c8e9f31e97e0f93d9ac8bdb4791fb69c
vtrifonov548 <vtrifonov548@gmail.com>
2020-10-19 01:44:28 +0300

    Добавление компонента LessonHeader (шапка занятия)

---
+++ EducationalHelp.Client/src/pages/LessonSinglePage/components/LessonHeader/index.ts
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+import CreatedUpdatedInfo from "@/components/CreatedUpdatedInfo/index.vue";
+
+// @ts-ignore
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// @ts-ignore
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import Lesson from "@/api/models/Lesson";
+
+
+@Component({
+    components: {
+      'created-updated-info': CreatedUpdatedInfo,
+      'ckeditor': CKEditor.component
+    }
+})
+export default class LessonHeader extends Vue {
+    public headEditing: boolean = false;
+    public editor: any = ClassicEditor;
+
+    @Prop({type: Object as () => Lesson})
+    public lesson!: Lesson;
+
+    public startDate: string = '';
+    public startTime: string = '';
+    public endDate: string = '';
+    public endTime: string = '';
+
+    constructor() {
+        super();
+    }
+
+    // Необходимо использовать watch за входящ. параметром lesson, т.к. он приходит асинхронно (не в момент загрузки компонента)
+    @Watch("lesson")
+    initalizeDateAndTime() {
+        // @ts-ignore
+        let splittedDateStart = this.lesson.dateStart.split("T");
+        this.startDate = splittedDateStart[0];
+        this.startTime = splittedDateStart[1];
+        // @ts-ignore
+        let splittedDateEnd = this.lesson.dateEnd.split("T");
+        this.endDate = splittedDateEnd[0];
+        this.endTime = splittedDateEnd[1];
+    }
+
+    @Watch("startDate")
+    @Watch("startTime")
+    updateDateStart() {
+        this.$props.lesson.dateStart = this.startDate + "T" + this.startTime;
+    }
+
+    @Watch("endDate")
+    @Watch("endTime")
+    updateDateEnd() {
+        this.$props.lesson.dateEnd = this.endDate + "T" + this.endTime;
+    }
+}
---
+++ EducationalHelp.Client/src/pages/LessonSinglePage/components/LessonHeader/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="card header-card">
+        <div class="card-body">
+          <div class="row justify-content-between">
+            <div class="col col-md-auto">
+              <div class="mb-3" v-if="!headEditing">
+                <h5 class="card-title">
+                  {{lesson.title}}
+                  <span class="badge badge-secondary">
+                    {{lesson.label}}
+                  </span>
+                </h5>
+                <p class="card-text" v-html="lesson.description"></p>
+              </div>
+              <div class="mb-3" v-else>
+                <b-input-group>
+                  <b-input-group-prepend>
+                    <b-input-group-text>
+                        Название и метка
+                    </b-input-group-text>
+                  </b-input-group-prepend>
+                  <b-form-input v-model="lesson.title"></b-form-input>
+                  <b-form-input v-model="lesson.label"></b-form-input>
+                </b-input-group>
+                <div class="pt-2">
+                  <ckeditor :editor="editor" class="ckeditor-size" v-model="lesson.description" :config="{language: 'ru'}"></ckeditor>
+                </div>
+              </div>
+              <created-updated-info :entity="lesson"></created-updated-info>
+            </div>
+            <div class="col col-md-auto">
+              <div v-if="!headEditing">
+                <p class="mb-0">Начало занятия: {{lesson.dateStart}}</p>
+                <p>Конец занятия: {{lesson.dateEnd}}</p>
+              </div>
+              <div class="col col-md-auto" v-else>
+                <div class="mb-4">
+                  <b-input-group-text class="mb-1">
+                     Начало занятия
+                  </b-input-group-text>
+                  <b-input-group prepend="Дата" class="mb-1">
+                    <b-form-datepicker v-model="startDate"></b-form-datepicker>
+                  </b-input-group>
+                  <b-input-group prepend="Время">
+                    <b-form-timepicker v-model="startTime"></b-form-timepicker>
+                  </b-input-group>
+                </div>
+                <div>
+                  <b-input-group-text class="mb-1">
+                    Конец занятия
+                  </b-input-group-text>
+                  <b-input-group prepend="Дата" class="mb-1">
+                    <b-form-datepicker v-model="endDate"></b-form-datepicker>
+                  </b-input-group>
+                  <b-input-group prepend="Время">
+                    <b-form-timepicker v-model="endTime"></b-form-timepicker>
+                  </b-input-group>
+                </div>
+              </div>
+              <div class="row justify-content-end">
+                <b-button v-if="!headEditing" variant="outline-primary" @click="headEditing = !headEditing"><b-icon-pencil-square></b-icon-pencil-square> Редактировать шапку</b-button>
+                <b-button v-else variant="outline-success btn-save" @click="headEditing = !headEditing"><b-icon-check></b-icon-check> Сохранить</b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./index.ts">
+
+</script>
+
+<style scoped src="./index.css">
+
+</style>
