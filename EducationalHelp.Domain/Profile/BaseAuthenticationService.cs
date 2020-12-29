0394bb174a3fa5adc6b6d931bc60db28586b6c82
VladislavTrifonov <vtrifonov548@gmail.com>
2020-12-29 04:34:53 +0300

    Добавлен базовый сервис авторизации

---
+++ EducationalHelp.Domain/Profile/BaseAuthenticationService.cs
@@ -0,0 +1,69 @@
+﻿using EducationalHelp.Core.Entities;
+using EducationalHelp.Data;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using EducationalHelp.Services.Exceptions;
+
+namespace EducationalHelp.Services.Profile
+{
+    public abstract class BaseAuthenticationService
+    {
+        private readonly IRepository<User> _usersRepository;
+
+        public BaseAuthenticationService(IRepository<User> usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        protected bool CheckCredentials(UserCredentials credentials)
+        {
+            var user = _usersRepository.AllData.FirstOrDefault(u => u.Pseudonym.Equals(credentials.Pseudonym, StringComparison.InvariantCulture));
+            if (user == null)
+            {
+                throw new ResourceNotFoundException($"User with pseudonym \"{credentials.Pseudonym}\" wasn't found");
+            }
+
+            var hashedPass = BaseAuthenticationService.GetSHA256(credentials.Password);
+
+            return user.PassHash.Equals(hashedPass, StringComparison.Ordinal);
+        }
+
+        protected User UpdateCredentials(User user, UserCredentials credentials)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            if (String.IsNullOrEmpty(credentials.Password))
+            {
+                throw new ArgumentNullException(nameof(credentials.Password));
+            }
+
+            if (String.IsNullOrEmpty(credentials.Pseudonym))
+            {
+                throw new ArgumentNullException(nameof(credentials.Pseudonym));
+            }
+
+            user.Pseudonym = credentials.Pseudonym;
+            user.PassHash = GetSHA256(credentials.Password);
+
+            return user;
+        }
+
+        private static string GetSHA256(string text)
+        {
+            var encoding = new UTF8Encoding();
+            var textBytes = encoding.GetBytes(text);
+            byte[] hashBytes;
+
+            var hash = new SHA256Managed();
+            hashBytes = hash.ComputeHash(textBytes);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ EducationalHelp.Domain/Profile/UserCredentials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalHelp.Services.Profile
+{
+    public struct UserCredentials
+    {
+        public string Pseudonym { get; set; }
+        public string Password { get; set; }
+
+        public UserCredentials(string login, string password)
+        {
+            Pseudonym = login;
+            Password = password;
+        }
+    }
+}
