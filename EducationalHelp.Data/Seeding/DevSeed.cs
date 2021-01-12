f3745571e1be3428699b8c32e66fd00896bae405
VladislavTrifonov <vtrifonov548@gmail.com>
2021-01-12 19:28:20 +0300

    Обновлены данные по умолчанию

--- EducationalHelp.Data/Seeding/DevSeed.cs
+++ EducationalHelp.Data/Seeding/DevSeed.cs
@@ -16,14 +16,19 @@ namespace EducationalHelp.Data.Seeding
             testLessons.Add(new Lesson() 
             { 
                 Id = new Guid("0B5A3D64-394F-4EB0-BD82-D6C9F068BADE"), 
-                SubjectId = new Guid("FE3E3D54-E538-47AF-914B-F7B621F395F5"), 
+                SubjectId = new Guid("7623BD52-72F5-42D1-BF85-11A62D9B19A7"), 
                 DateStart = DateTime.Now, 
-                DateEnd = DateTime.Now.AddDays(3), 
-                Description = "Test", 
-                Homework = "nichego", 
-                Label = "edu", 
-                Notes = "nothing", 
-                Title = "test lesson"});
+                DateEnd = DateTime.Now.AddHours(1), 
+                Description = @"Тема 1.1. Числовые множества и
+                                операции над ними.Ограниченность
+                                множества, точная верхняя и нижняя
+                                грань.Числовые последовательности,
+                                предел последовательности.
+                                ", 
+                Homework = "Задачи 1-10", 
+                Label = "Практика", 
+                Notes = "", 
+                Title = "Вводное занятие"});;
 
             var testFile = new File()
             {
@@ -42,11 +47,7 @@ namespace EducationalHelp.Data.Seeding
 
             var subjects = new List<Subject>()
             {
-                new Subject() { Id = new Guid("FE3E3D54-E538-47AF-914B-F7B621F395F5"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Алгоритмы и алгоритмические языки", Teacher="Горюнов Ю.Ю.", Description="алгоритмы"},
-                new Subject() { Id = new Guid("7623BD52-72F5-42D1-BF85-11A62D9B19A7"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Математический анализ (1 семестр)", Teacher="Васюнина О.Б.", Description="матан"},
-                new Subject() { Id = new Guid("30100ECF-741C-4B64-B842-6EAEF3D60B2B"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Математический анализ (2 семестр)", Teacher="Хорошева Э.А.", Description="матан"},
-                new Subject() { Id = new Guid("8E76B156-CA17-48EC-9D9F-FF9ED066F41D"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Программирование (1 семестр)", Teacher="Шибанов С.В.", Description="прогр-е"},
-                new Subject() { Id = new Guid("5EE8DB95-8284-40EE-9D36-8E172FD9B1FD"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Программирование (2 семестр)", Teacher="Шибанов С.В.", Description="прогр-е"},
+                new Subject() { Id = new Guid("7623BD52-72F5-42D1-BF85-11A62D9B19A7"), UserId = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"), GroupId = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"), Name="Математический анализ", Teacher="Иванов И.И.", Description="Вводный курс математического анализа"},
             };
 
             var users = new List<User>()
@@ -54,22 +55,22 @@ namespace EducationalHelp.Data.Seeding
                 new User() 
                 { 
                     Id = new Guid("331DF5C7-9FBE-45E4-AF6D-02AFCFCB9C1D"),
-                    Pseudonym = "Жмышенко Валерий",
-                    Login = "admin",
+                    Pseudonym = "Петров Петр",
+                    Login = "petrov",
                     PassHash = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
                 },
                 new User()
                 {
                     Id = new Guid("3EBFBF90-5644-42FE-9CB6-1D0F0ED668BF"),
-                    Pseudonym = "Жмышенко Альберт",
-                    Login = "batya_admina",
+                    Pseudonym = "Иванов Иван",
+                    Login = "ivanov",
                     PassHash = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
                 },
                 new User()
                 {
                     Id = new Guid("E22975CA-C262-48A9-8474-E3D0811FA027"),
-                    Pseudonym = "Богданов Евгений",
-                    Login = "bogdanov",
+                    Pseudonym = "Сидоров Сидр",
+                    Login = "sidorov",
                     PassHash = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
                 }
                 
@@ -80,14 +81,14 @@ namespace EducationalHelp.Data.Seeding
                 new Group()
                 {
                     Id = new Guid("B5BF64C2-133C-4F06-9478-60EA97166AF6"),
-                    Title = "19ВМ1_ПГУ",
-                    Description = "Тестовая группа"
+                    Title = "Математики",
+                    Description = "Группа любителей математики"
                 },
                 new Group()
                 {
                     Id = new Guid("7B6B70CB-72B4-4508-8978-B8DD2391404F"),
-                    Title = "54ГМ2_ГГУ",
-                    Description = "Тестовая группа 2"
+                    Title = "Историки",
+                    Description = "Группа любителей истории"
                 }
             };
 
